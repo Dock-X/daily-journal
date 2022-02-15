3f668eeffa8940b2f22b5d4864d3190954a3ce3b
William Dockery <williamdockery98@gmail.com>
2022-02-14 20:31:17 -0600

    Added entries.js, dailyjournal.js, database.json - api

---
+++ scripts/DailyJournal.js
@@ -0,0 +1,15 @@
+import { Entries } from "./Entries.js"
+
+export const DailyJournal = () => {
+    return `
+        <h1>Daily Journal</h1>
+
+        <div class="entryList">
+            ${ Entries() }
+        </div>
+
+        <article class="entryForm">
+            Use interpolation to put the HTML that the form function generates here
+        </article>
+    `
+}
